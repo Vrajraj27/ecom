0e60772c3f864e7fb8da975930bb6d8353f34f2a
Vrajraj27 <vrajraj27shekhada@gmail.com>
2022-09-21 20:02:28 +0530

    live

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(function (err, req, res, next) {
     res.json({ success: false, message: '500 Server Error', data: err.stack });
     next(err);
 });
-let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log('Server is up and running on ${PORT} ...');
 })
